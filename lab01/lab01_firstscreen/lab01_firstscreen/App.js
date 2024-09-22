a1a7636730511bffc3e3be864f7cf2094a948c90
vantoan24-iuh <vantoan24-iuh>
2024-09-22 15:47:15 +0700

    lab01 firstscreen

---
+++ lab01/lab01_firstscreen/lab01_firstscreen/App.js
@@ -0,0 +1,31 @@
+
+import React from 'react';
+import {Text, View,Button,Image} from 'react-native';
+
+const YourApp = () => {
+  return (
+    <View style={{flex:1,backgroundColor:'#00ccf9',width:360, height:640}}>
+      <View
+        style={{
+          flex: 2,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <View style={{width: 140,height: 140,borderRadius: 140 / 2,borderColor: 'black',borderWidth: 15,}} />
+      </View> 
+      <View style={{flex: 1,justifyContent:'center', alignItems:'center'}}>
+        <Text style={{fontSize:20,fontWeight:'bold',textTransform:'uppercase', width:189,height:58,textAlign:'center'}}>grow {"\n"} your business</Text>
+      </View>
+      <View style={{flex: 1,justifyContent:'center', alignItems:'center'}}>
+        <Text style={{fontSize:15,fontWeight:'bold',textAlign:'center',width:302,height:36}}>we will help you to grow your business using online servers</Text>
+      </View>
+      <View style={{flex: 1,flexDirection:'row',justifyContent:'space-around', alignItems:'center'}}>
+        <button style={{backgroundColor:'#e3c000',fontSize:24,textTransform:'uppercase', borderRadius:8,width:119,height:48}}>Login</button>
+
+        <button style={{backgroundColor:'#e3c000',fontSize:24,textTransform:'uppercase',borderRadius:8,width:119,height:48}}>Sign up</button>
+      </View>
+    </View>
+  );
+};
+
+export default YourApp;
